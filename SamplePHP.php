38868476caea43c8a1fddee3b15f40341b5b0fa0
skattimath <70135365+skattimath@users.noreply.github.com>
2021-04-10 17:51:23 +0530

    new setup test

--- SamplePHP.php
+++ SamplePHP.php
@@ -1,5 +1,5 @@
 #Test############# 1 2 3 4 5 6 7 8 9 10 11 12 - Set PINK
-<?php include "../inc/dbinfo.inc"; ?>    
+<?php include "../inc/dbinfo.inc"; ?>     
 <html>  
 #<body>
 <body style="background-color:pink">
